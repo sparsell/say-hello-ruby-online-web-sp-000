f8f6b4d4ff92c9139a9ced4d1113bcded7859dbc
Sonja Parsell <hello@sparsell.com>
2020-03-05 18:46:35 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
 # Build your say_hello method here
-def say_hello(name = "Hello Ruby programmer!")
-  puts "Hello #{name}!"
+def say_hello(name="Hello Ruby programmer!")
+  puts "name}!"
 end
 
 say_hello
